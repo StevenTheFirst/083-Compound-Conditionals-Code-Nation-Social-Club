29ca9c9abd3ea8e9f6028d7b203de9fad8df0622
StevenTheFirst <72420342+StevenTheFirst@users.noreply.github.com>
2020-10-26 16:55:30 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+// set the correct username and password combination below
+var correctUsername = "user";
+var correctPassword = "pass";
+
+
+$("button").click(function() {
+    var username = $(".username").val();
+    var password = $(".password").val();
+    
+    // WRITE YOUR COMPOUND CONDITIONAL BELOW THIS LINE
+ 
+ if(username===correctUsername){
+     $(".username");
+}
+if(password===correctPassword){
+    $(".password");
+    }
+
+});
